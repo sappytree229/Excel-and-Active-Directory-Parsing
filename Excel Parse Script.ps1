d3d1f0df6ed2e0c3c02f77895671eacb2300cd5f
sappytree229 <95254831+sappytree229@users.noreply.github.com>
2022-07-22 14:10:23 -0500

    Add files via upload

---
+++ Excel Parse Script.ps1
@@ -0,0 +1,32 @@
+﻿$ExcelFile = '.\Calabrio Clean Up.xlsx'
+$ExcelSheet = 'Calabrio'
+$CSVFile = '.\Calabrio Clean Up - Working.csv'
+
+$ImportedFile = Import-Excel -path $ExcelFile -WorksheetName $ExcelSheet | Where-Object `
+{ $_."Employee ID" -like $null `
+        -and $_."Deactivated" -like $null `
+        -and $_."First Name" -notlike $null `
+        -and $_."Last Name" -notlike $null }
+
+Import-Excel `
+    -path $ExcelFile `
+    -WorksheetName $ExcelSheet | Export-Csv `
+    -path $CSVFile
+
+$FixedCSV = Import-Csv -path $CSVFile
+
+foreach ($User in $ImportedFile) {
+    $Name = "$($User."First Name") $($User."Last Name")"
+
+    $UserInfo = Get-ADUser -Filter "Name -eq '$Name' -and Enabled -eq 'True'" -Properties mail, EmployeeID, SAMAccountName, givenName, sn `
+    | Select-Object givenName, sn, mail, EmployeeID, `
+    @{l="SAMAccountName";e={"HQ\" + $_.SAMAccountName}}
+
+    $FixedCSV | Where-Object {"$($_."Login")" -eq "$($UserInfo.SAMAccountName)"} | foreach {
+        $_."Employee ID" = $UserInfo.EmployeeID
+        $_."Email" = $UserInfo.mail
+        $_."Login" = $UserInfo.SAMAccountName
+    } 
+}
+
+$FixedCSV | Export-Excel -Path '.\Calabrio Clean Up - Separate Names.xlsx' -WorksheetName $ExcelSheet
---
+++ Parse AD for Email.ps1
@@ -0,0 +1,30 @@
+﻿$ExcelFile = '.\Calabrio Clean Up- Finished.xlsx'
+$ExcelSheet = 'Calabrio'
+$CSVFile = '.\AD Email Query.csv'
+
+$ImportedFile = Import-Excel -path $ExcelFile -WorksheetName $ExcelSheet | Where-Object `
+{ $_."Employee ID" -like $null `
+        -and $_."Deactivated" -like $null `
+        -and $_."First Name" -notlike $null `
+        -and $_."Last Name" -notlike $null }
+
+Import-Excel `
+    -path $ExcelFile `
+    -WorksheetName $ExcelSheet | Export-Csv `
+    -path $CSVFile
+
+$FixedCSV = Import-Csv -path $CSVFile
+
+foreach ($User in $ImportedFile) {
+    $Email = $User."Email"
+
+    $UserInfo = Get-ADUser -Filter {UserPrincipalName -eq "$Email"} -Properties mail, EmployeeID, SAMAccountName, UserPrincipalName `
+    | Select-Object UserPrincipalName, EmployeeID, @{l="SAMAccountName";e={"HQ\" + $_.SAMAccountName}}
+
+    $FixedCSV | Where-Object {$Email -eq $UserInfo.EmailAddress} | ForEach {
+        $_."Employee ID" = $UserInfo.EmployeeID 
+        $_."Login" = $UserInfo.SAMAccountName
+    } 
+}
+
+$FixedCSV | Export-Excel -Path '.\AD 2nd Fix Calabrio Clean Up- Finished.xlsx' -WorksheetName $ExcelSheet
